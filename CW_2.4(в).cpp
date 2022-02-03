cfebca13d148167d520adad6b415d8ac1369a9c5
liashenkokate <78730716+liashenkokate@users.noreply.github.com>
2022-02-03 12:01:34 +0200

    Classwork2: Liashenko Kate

---
+++ CW_2.4(в).cpp
@@ -0,0 +1,14 @@
+﻿// CW_2.4(в)
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	float x, x2, x3, x4, x5, y;
+	printf("x=");
+	scanf_s("%f", &x);
+	x2 = x * x;
+	x3 = x2 * x;
+	x4 = x2 * x2;
+	x5 = x4 * x;
+	y = x5 + 5 * x4 + 10 * x3 + 10 * x2 + 5 * x + 1;
+	printf("Answer = %0.1f", y);
+}
