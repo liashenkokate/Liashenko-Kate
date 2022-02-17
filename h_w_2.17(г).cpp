c67eace3bd7132299086a3a50964c911a4e140d1
liashenkokate <78730716+liashenkokate@users.noreply.github.com>
2022-02-17 12:27:28 +0200

    HW3 #2.17(г) Liashenko Kateryna

---
+++ h_w_2.17(г).cpp
@@ -0,0 +1,19 @@
+﻿// 2.17(г).cpp
+#include <stdio.h>
+#include <cmath>
+double tan_f(double x){
+	double y;
+	y = 1 / tan(x);
+	return y;
+}
+double derivative(double x){
+	double y, k;
+	k = -1 * (1 / (sin(x)));
+	y = pow(k, 2);
+	return y;
+}
+int main() {
+	printf("Function in x0 is:%f, derivative in this x0 is: %lf\n", tan_f(-2), derivative(-2));
+	printf("Function in x0 is:%f, derivative in this x0 is:%lf\n", tan_f(0), derivative(0));
+	printf("Function in x0 is:%f, derivative in this x0 is: %lf\n", tan_f(1), derivative(1));
+}
